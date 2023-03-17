2cf57e9d5e8efa2d2e43458e9b73141743da7185
marco <macmwz20@gmail.com>
2023-03-17 15:10:10 +1300

    react <3

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express')
 
 const productRoutes = require('./routes/productRoutes');
 
+const swaggerUi = require('swagger-ui-express');
+swaggerDocument = require('./swagger.json');
+
 const app = express()
 const port = 3000
 
 app.use('/', express.static('public'));
 
 app.use('/products', productRoutes);
+
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+  );
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
